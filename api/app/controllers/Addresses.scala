bf4a4fd1417b322031458d66c5158eb583fae6d7
Michael Bryzek <mbryzek@alum.mit.edu>
2016-09-26 18:31:06 -0400

    Stub implementation of address verification

--- api/app/controllers/Addresses.scala
+++ api/app/controllers/Addresses.scala
@@ -3,8 +3,10 @@ package controllers
 import play.api.libs.json._
 import play.api.mvc._
 import io.flow.play.util.Validation
+import io.flow.common.v0.models.Address
 import io.flow.common.v0.models.json._
-import scala.concurrent.Future
+import io.flow.location.v0.models.{AddressVerification, AddressSuggestion}
+  import scala.concurrent.Future
 
 @javax.inject.Singleton
 class Addresses @javax.inject.Inject() (
@@ -22,6 +24,27 @@ class Addresses @javax.inject.Inject() (
         case Left(_) => UnprocessableEntity(Json.toJson(Validation.error("Must specify either 'address' or 'ip'")))
         case Right(locations) => Ok(Json.toJson(locations))
       }
+    }
+  }
+
+  def postVerifications() = Action.async(parse.json) { request =>
+    val address = request.body.as[Address]
+    Future {
+      Ok(
+        AddressVerification(
+          address = address,
+          valid = true,
+          suggestions = Nil
+        )
+      )
+      /*
+      helpers.getLocations(address = address) match {
+        case Left(errors) => sys.error(s"Error in address verification: $errors")
+        case Right(locations) => {
+          // TODO: Diff locations
+        }
+      }
+       */
     }
   }
 
