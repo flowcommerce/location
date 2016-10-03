006dc046d39bc2d229d2296217033466a579b17b
Michael Bryzek <mbryzek@alum.mit.edu>
2016-10-03 15:12:30 -0400

    Undo google street merge if just for house number (and no change)

--- api/app/utils/AddressVerifier.scala
+++ api/app/utils/AddressVerifier.scala
@@ -31,11 +31,40 @@ object AddressVerifier {
       }
     }
 
+    val merged = collapseStreets(address, matched)
+
     AddressVerification(
       address = address,
       valid = isValid,
-      suggestions = toSuggestions(address, matched)
+      suggestions = toSuggestions(address, merged)
     )
+  }
+
+  /**
+    * Our clients list streets as '123 main st' - annoying to have google return as ['123', 'main st']
+    */
+  private[utils] def collapseStreets(master: Address, addresses: Seq[Address]): Seq[Address] = {
+    master.streets.getOrElse(Nil).headOption.map { v => v.trim.split("\\s+").head } match {
+      case None => addresses
+      case Some(prefix) => {
+        addresses.map { a =>
+          a.streets.getOrElse(Nil).headOption == Some(prefix) match {
+            case false => a
+            case true => {
+              a.copy(
+                streets = a.streets.getOrElse(Nil) match {
+                  case Nil => None
+                  case a :: Nil => Some(Seq(a))
+                  case a :: b :: rest => {
+                    Some(Seq(a + " " + b) ++ rest)
+                  }
+                }
+              )
+            }
+          }
+        }
+      }
+    }
   }
 
   private[utils] def toSuggestions(address: Address, matched: Seq[Address]): Seq[AddressSuggestion] = {
