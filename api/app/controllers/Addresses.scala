afbbe8dfffe396ca8117abb8c9f780768f822b3a
Ben Roberson <broberson@flow.io>
2018-01-11 11:09:46 -0500

    Revert "Add Graphite timer for Address controller methods. (#225)"
    This reverts commit 9d27e2acbf92fe489571972ab117fa21b59e3bde.

--- api/app/controllers/Addresses.scala
+++ api/app/controllers/Addresses.scala
@@ -8,7 +8,6 @@ import io.flow.common.v0.models.Address
 import io.flow.common.v0.models.json._
 import io.flow.error.v0.models.json._
 import io.flow.location.v0.models.json._
-import io.flow.play.metrics.MetricsInstrumentation
 
 import scala.concurrent.Future
 import utils.AddressVerifier
@@ -17,43 +16,37 @@ import utils.AddressVerifier
 class Addresses @javax.inject.Inject() (
   helpers: Helpers,
   system: ActorSystem
-) extends Controller with MetricsInstrumentation {
+) extends Controller {
 
   private[this] implicit val ec = system.dispatchers.lookup("addresses-controller-context")
-  private[this] val getTimer = metrics.timer("get")
-  private[this] val postVerificationsTimer = metrics.timer("postVerifications")
 
   def get(
     address: Option[String],
     ip: Option[String]
   ) = Action.async { request =>
-    getTimer.timeFuture {
-      helpers.getLocations(address = address, ip = ip).map(addrs => addrs match {
-        case Left(error) => UnprocessableEntity(Json.toJson(error))
-        case Right(locations) => Ok(Json.toJson(locations))
-      })
-    }
+    helpers.getLocations(address = address, ip = ip).map( addrs => addrs match {
+      case Left(error) => UnprocessableEntity(Json.toJson(error))
+      case Right(locations) => Ok(Json.toJson(locations))
+    })
   }
 
   def postVerifications() = Action.async(parse.json) { request =>
-    postVerificationsTimer.timeFuture {
-      val address = request.body.as[Address]
-      AddressVerifier.toText(address) match {
-        case None => {
-          Future.successful(UnprocessableEntity(Json.toJson(Validation.error("Address to verify cannot be empty"))))
-        }
+    val address = request.body.as[Address]
+    AddressVerifier.toText(address) match {
+      case None => {
+        Future.successful ( UnprocessableEntity(Json.toJson(Validation.error("Address to verify cannot be empty"))) )
+      }
 
-        case Some(text) => {
-          helpers.getLocations(address = Some(text)).map(addrs => addrs match {
-            case Left(errors) => {
-              sys.error(s"Error in address verification: $errors")
-            }
+      case Some(text) => {
+        helpers.getLocations(address = Some(text)).map ( addrs => addrs match {
+          case Left(errors) => {
+            sys.error(s"Error in address verification: $errors")
+          }
 
-            case Right(locations) => {
-              Ok(Json.toJson(AddressVerifier(address, locations)))
-            }
-          })
-        }
+          case Right(locations) => {
+            Ok(Json.toJson(AddressVerifier(address, locations)))
+          }
+        })
       }
     }
   }
