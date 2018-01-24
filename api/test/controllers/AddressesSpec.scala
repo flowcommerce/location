c20801cf930c8753db221cd05198b49ed45c7f28
Michael Bryzek <mbryzek@alum.mit.edu>
2018-01-24 09:42:36 -0500

    Update DI in tests (#232)

--- api/test/controllers/AddressesSpec.scala
+++ api/test/controllers/AddressesSpec.scala
@@ -1,10 +1,9 @@
 package controllers
 
 import io.flow.common.v0.models.Address
-import org.scalatestplus.play._
 import play.api.libs.json.{JsError, JsSuccess, Json}
 import play.api.test.Helpers._
-import play.api.test._
+import org.scalatestplus.play.{OneServerPerSuite, PlaySpec}
 import io.flow.location.v0.Client
 import io.flow.common.v0.models.json._
 
@@ -14,24 +13,21 @@ class AddressesSpec extends PlaySpec with OneServerPerSuite with TestHelpers {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  implicit override lazy val port = 9010
-  implicit override lazy val app: FakeApplication = FakeApplication()
-
   lazy val client = new Client(s"http://localhost:$port")
 
-  "GET /addresses" in new WithServer {
+  "GET /addresses" in {
     expectStatus(422) {
       client.addresses.get()
     }
   }
 
-  "GET /addresses?address=sample" in new WithServer {
+  "GET /addresses?address=sample" in {
     expectStatus(422) {
       client.addresses.get(address = Some("sample"))
     }
   }
 
-  "GET /addresses?ip=23.16.0.0" in new WithServer {
+  "GET /addresses?ip=23.16.0.0" in {
     val locations = await(
       client.addresses.get(ip = Some("23.16.0.0"))
     )
@@ -43,7 +39,7 @@ class AddressesSpec extends PlaySpec with OneServerPerSuite with TestHelpers {
     }
   }
 
-  "POST /addresses/verifications" in new WithServer {
+  "POST /addresses/verifications" in {
     expectErrors(
       client.addresses.postVerifications(address = Address())
     ).genericError.messages must be(
@@ -51,7 +47,7 @@ class AddressesSpec extends PlaySpec with OneServerPerSuite with TestHelpers {
     )
   }
 
-  "POST /addresses/verifications with UK address" in new WithServer {
+  "POST /addresses/verifications with UK address" in {
     val result = await(
       client.addresses.postVerifications(address = Address(text = Some("76 Belsize Park NW3-4NG")))
     )
