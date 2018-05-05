7f7c95364b9e592c561411c5ada12a715635e612
Michael Bryzek <mike@flow.io>
2018-05-04 21:36:40 -0400

    wip

--- api/test/controllers/TimezonesSpec.scala
+++ api/test/controllers/TimezonesSpec.scala
@@ -1,12 +1,8 @@
 package controllers
 
-import io.flow.common.v0.models.Address
-import io.flow.common.v0.models.json._
 import io.flow.location.v0.Client
-import io.flow.reference.data.Timezones
 import io.flow.test.utils.FlowPlaySpec
-import play.api.libs.json.{JsError, JsSuccess, Json}
-import utils.DigitalElement
+import utils.DigitalElementSampleData
 
 class TimezonesSpec extends FlowPlaySpec with TestHelpers {
 
@@ -21,19 +17,12 @@ class TimezonesSpec extends FlowPlaySpec with TestHelpers {
   }
 
   "GET /addresses?ip=23.16.0.0" in {
-    val data = Map(
-      "37.187.99.146" -> Timezones.EuropeParis,
-      "23.16.0.0" -> Timezones.AmericaEdmonton,
-      "185.236.200.99" -> Timezones.AmericaLosAngeles,
-      "173.63.71.187" -> Timezones.AmericaNewYork
-    )
-
-    val invalid = data.keys.toSeq.flatMap { ip =>
+    val invalid = DigitalElementSampleData.IpTimezones.keys.toSeq.flatMap { ip =>
       val timezones = await(
         client.timezones.get(ip = Some("23.16.0.0"))
       )
       println(s"Timezones: $timezones")
-      Some(s"Expected ip address '$ip' to resolve to timezone '${data(ip)}' but found '$timezones'")
+      Some(s"Expected ip address '$ip' to resolve to timezone '${DigitalElementSampleData.IpTimezones(ip)}' but found '$timezones'")
     }
 
     invalid must equal(Nil)
--- api/test/utils/DigitalElementIndexSpec.scala
+++ api/test/utils/DigitalElementIndexSpec.scala
@@ -2,8 +2,6 @@ package utils
 
 import java.io._
 import java.nio.file.{Files, Paths}
-
-import io.flow.reference.data.Timezones
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.util.{Failure, Success}
@@ -30,14 +28,7 @@ class DigitalElementIndexSpec extends WordSpec with Matchers {
     }
 
     "timezone" in {
-      val data = Map(
-        "37.187.99.146" -> Timezones.EuropeParis,
-        "23.16.0.0" -> Timezones.AmericaEdmonton,
-        "185.236.200.99" -> Timezones.AmericaLosAngeles,
-        "173.63.71.187" -> Timezones.AmericaNewYork
-      )
-
-      val invalid = data.keys.toSeq.flatMap { ip =>
+      val invalid = DigitalElementSampleData.IpTimezones.keys.toSeq.flatMap { ip =>
         val ipInt = DigitalElement.ipToDecimal(ip) match {
           case Failure(_) => sys.error(s"Missing ip '$ip'")
           case Success(v) => v
@@ -51,10 +42,10 @@ class DigitalElementIndexSpec extends WordSpec with Matchers {
           sys.error(s"ip '$ip' missing timezone")
         }
 
-        if (tz == data(ip)) {
+        if (tz == DigitalElementSampleData.IpTimezones(ip)) {
           None
         } else {
-          Some(s"IP '$ip' expected timezone[${data(ip).name}] but got '${tz.name}'")
+          Some(s"IP '$ip' expected timezone[${DigitalElementSampleData.IpTimezones(ip).name}] but got '${tz.name}'")
         }
       }
       invalid should equal(Nil)
