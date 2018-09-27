83df550e7049b44b2ed6e3ad9ab9a8a14fb353ad
Kevin Grant <kevingrant@Flows-iPad.localdomain>
2018-09-27 16:41:02 -0400

    fixing tests

--- api/test/controllers/TestHelpers.scala
+++ api/test/controllers/TestHelpers.scala
@@ -10,7 +10,7 @@ trait TestHelpers {
 
   val DefaultDuration = Duration(1, TimeUnit.SECONDS)
 
-  def expectStatus[T](code: Int)(f: => Future[T]) {
+  def expectStatus[T](code: Int)(f: => Future[T]): Unit = {
     assert(code >= 400, s"code[$code] must be >= 400")
 
     Try(
@@ -18,16 +18,20 @@ trait TestHelpers {
     ) match {
       case Success(_) => {
         org.specs2.execute.Failure(s"Expected HTTP[$code] but got HTTP 2xx")
+        ()
       }
       case Failure(ex) => ex match {
         case UnitResponse(c) if c == code => {
           org.specs2.execute.Success()
+          ()
         }
         case UnitResponse(c) => {
           org.specs2.execute.Failure(s"Expected code[$code] but got[$c]")
+          ()
         }
         case e => {
           org.specs2.execute.Failure(s"Unexpected error: $e")
+          ()
         }
       }
     }
