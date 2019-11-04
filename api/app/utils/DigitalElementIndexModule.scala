d376f444254f2c7e84c3151a41cea162db0c7964
Jean Logeart <jean.logeart@gmail.com>
2019-11-04 11:26:02 -0500

    Revert "Use RangeMap instead of custom structure" (#385)
    This reverts commit e018801a879ffb403db30a241f2691526f5e5cfd.

--- api/app/utils/DigitalElementIndexModule.scala
+++ api/app/utils/DigitalElementIndexModule.scala
@@ -35,14 +35,14 @@ class DigitalElementIndexModule extends AbstractModule {
       withKeyValue("uri", environmentVariables.digitalElementFileUri).
       info(s"Building index")
     val start = System.currentTimeMillis()
-    val index = DigitalElement.buildIndex(is, '\n')
+    val index = DigitalElement.buildIndex(is, ';', '\n')
     is.close()
 
     val elapsedTime = (System.currentTimeMillis() - start) / 1000
     logger.
       fingerprint(getClass.getName).
       withKeyValue("uri", environmentVariables.digitalElementFileUri).
-      withKeyValue("size", index.asMapOfRanges().size()).
+      withKeyValue("size",index.size).
       withKeyValue("elapsed_seconds", elapsedTime).
       info("Indexed Records")
     index
