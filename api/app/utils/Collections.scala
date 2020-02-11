4e72affeed6a7fe64d9a7f4771102118cb2e7cd8
Jean Logeart <jean.logeart@gmail.com>
2020-02-11 09:37:02 -0500

    Use views

--- api/app/utils/Collections.scala
+++ api/app/utils/Collections.scala
@@ -1,27 +1,33 @@
 package utils
 
+import scala.annotation.tailrec
+import scala.collection.IndexedSeqView
+
 object Collections {
 
   /**
-    * Simple binary search that finds the closest element (relative to the head of a Sequence) that satisfies a boundary condition
-    * @param elems the sorted sequence to search
-    * @param boundary boundary value
-    * @param candidate the last element we have found that satisfies the boundary condition
-    * @param compare a function that will compare each element in elems to the boundary
-    * @return The element if found, None otherwise
-    */
+   * Simple binary search that finds the closest element (relative to the head of a Sequence) that satisfies a boundary condition
+   * @param elems the sorted sequence to search
+   * @param boundary boundary value
+   * @param candidate the last element we have found that satisfies the boundary condition
+   * @param compare a function that will compare each element in elems to the boundary
+   * @return The element if found, None otherwise
+   */
   def searchWithBoundary[A, B](elems: IndexedSeq[A], boundary: B, candidate: Option[A] = None)(implicit compare: (A, B) => Boolean): Option[A] =
-    elems match {
-      case IndexedSeq() => candidate
-      case IndexedSeq(x) if compare(x, boundary) => Some(x)
-      case _ => {
-        val mid = elems.length / 2
-        if (compare(elems(mid), boundary)) {
-          searchWithBoundary(elems.slice(mid+1, elems.length), boundary, Some(elems(mid)))
-        } else {
-          searchWithBoundary(elems.slice(0, mid), boundary, candidate)
-        }
-      }
+    searchWithBoundaryRec(elems.view, boundary, candidate)(compare)
+
+  @tailrec
+  private def searchWithBoundaryRec[A, B](elems: IndexedSeqView[A], boundary: B, candidate: Option[A])(implicit compare: (A, B) => Boolean): Option[A] =
+    if (elems.isEmpty)
+      candidate
+    else if (elems.sizeIs == 1 && elems.headOption.exists(compare(_, boundary)))
+      elems.headOption
+    else {
+      val mid = elems.length / 2
+      if (compare(elems(mid), boundary))
+        searchWithBoundaryRec(elems.slice(mid + 1, elems.length), boundary, Some(elems(mid)))
+      else
+        searchWithBoundaryRec(elems.slice(0, mid), boundary, candidate)
     }
 
 }
