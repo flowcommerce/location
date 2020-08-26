0f0ea5e65c9a21d461d12dc710e4d235a08e4b99
Jean Logeart <jean.logeart@gmail.com>
2020-08-26 15:32:12 -0400

    Revert "Use only the default context" (#405)

--- api/app/controllers/Healthchecks.scala
+++ api/app/controllers/Healthchecks.scala
@@ -1,18 +1,20 @@
 package controllers
 
+import akka.actor.ActorSystem
 import io.flow.healthcheck.v0.models.Healthcheck
 import io.flow.healthcheck.v0.models.json._
-import play.api.libs.json._
 import play.api.mvc._
-
-import scala.concurrent.ExecutionContext
+import play.api.libs.json._
 
 @javax.inject.Singleton
 class Healthchecks @javax.inject.Inject() (
   override val controllerComponents: ControllerComponents,
+  system: ActorSystem,
   environmentVariables: utils.EnvironmentVariables,
   addresses: Addresses
-)(implicit ec: ExecutionContext) extends BaseController {
+) extends BaseController {
+
+  private[this] implicit val ec = system.dispatchers.lookup("healthchecks-controller-context")
 
   def getHealthcheck() = Action.async { request =>
     // force loading of config
