c8555a11f753d35f9ceb8fad1074c2da9e67453d
Gregor Heine <gregor@flow.io>
2021-11-16 18:21:36 +0000

    build failure

--- api/app/utils/EnvironmentVariables.scala
+++ api/app/utils/EnvironmentVariables.scala
@@ -1,12 +1,10 @@
 package utils
 
 import io.flow.play.util.Config
-import play.api.{Environment, Mode}
 
 @javax.inject.Singleton
 class EnvironmentVariables @javax.inject.Inject() (
   config: Config,
-  playEnv: Environment
 ) {
 
   val googleApiKey: String = config.requiredString("google.api.key")
